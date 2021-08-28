4cd74d068e766934d57a1721d68151e2ad5cac83
Jens van Hellemondt <me@jvh.one>
2021-08-28 21:00:17 +0200

    feat: block and hash

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main () {
-    println!("Hello, blockchain!");
+use blockchainlib::*;
+
+fn main() {
+    let mut block = Block::new(0, now(), vec![0; 32], 0, "Genesis block".to_owned());
+
+
+    let h = block.hash();
+
+    block.hash = h;
+
+    println!("{:?}", &block);
 }
